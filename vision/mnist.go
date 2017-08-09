4ba627988b8b8281a31a3611f5c5d63ded67d396
Abdul Dakkak <abduld@wolfram.com>
2017-08-08 21:04:05 -0500

    add mnist

--- vision/mnist.go
+++ vision/mnist.go
@@ -1,58 +1,113 @@
 package vision
 
 import (
+	"bytes"
+	"io"
 	"path"
+	"strconv"
 	"strings"
 
 	context "golang.org/x/net/context"
 
+	"github.com/pkg/errors"
 	"github.com/rai-project/config"
 	"github.com/rai-project/dldataset"
 	mnistLoader "github.com/unixpickle/mnist"
 )
 
-type Mnist struct {
+type MNIST struct {
 	base
 	trainingData mnistLoader.DataSet
 	testData     mnistLoader.DataSet
 }
 
-var mnist *Mnist
+var mnist *MNIST
 
-func (*Mnist) Name() string {
-	return "Mnist"
+type MNISTLabeledImage struct {
+	label string
+	data  []byte
 }
 
-func (d *Mnist) CanonicalName() string {
+func (l MNISTLabeledImage) Label() string {
+	return l.label
+}
+
+func (l MNISTLabeledImage) Data() (io.Reader, error) {
+	return bytes.NewBuffer(l.data), nil
+}
+
+func (*MNIST) Name() string {
+	return "MNIST"
+}
+
+func (d *MNIST) CanonicalName() string {
 	category := strings.ToLower(d.Category())
 	name := strings.ToLower(d.Name())
 	key := path.Join(category, name)
 	return key
 }
 
-func (d *Mnist) New(ctx context.Context) (dldataset.Dataset, error) {
+func (d *MNIST) New(ctx context.Context) (dldataset.Dataset, error) {
 	return mnist, nil
 }
 
-func (d *Mnist) Download(ctx context.Context) error {
+func (d *MNIST) Download(ctx context.Context) error {
 	return nil
 }
 
-func (d *Mnist) List(ctx context.Context) ([]string, error) {
-	return nil, nil
+func (d *MNIST) List(ctx context.Context) ([]string, error) {
+	lst := []string{}
+	for ii := range d.trainingData.Samples {
+		lst = append(lst, "train/"+strconv.Itoa(ii))
+	}
+	for ii := range d.testData.Samples {
+		lst = append(lst, "test/"+strconv.Itoa(ii))
+	}
+	return lst, nil
 }
 
-func (d *Mnist) Get(ctx context.Context, name string) (dldataset.LabeledData, error) {
-	return nil, nil
+func (d *MNIST) Get(ctx context.Context, name string) (dldataset.LabeledData, error) {
+	var dataset mnistLoader.DataSet
+	if strings.HasPrefix(name, "train/") {
+		name = strings.TrimPrefix(name, "train/")
+		dataset = d.trainingData
+	} else if strings.HasPrefix(name, "test/") {
+		name = strings.TrimPrefix(name, "test/")
+		dataset = d.trainingData
+	} else {
+		return nil, errors.Errorf("cannot find %s in the mnist dataset", name)
+	}
+	idx, err := strconv.Atoi(name)
+	if err != nil {
+		return nil, errors.Errorf("expecting an integer, but got %s", name)
+	}
+	if idx >= len(dataset.Samples) {
+		return nil, errors.Errorf("the index %d is out of range %d", idx, len(dataset.Samples))
+	}
+
+	elem := dataset.Samples[idx]
+	data := make([]byte, len(elem.Intensities))
+	for ii, intensity := range elem.Intensities {
+		if intensity == 1 {
+			data[ii] = byte(1)
+		} else {
+			data[ii] = byte(0)
+		}
+	}
+
+	return &MNISTLabeledImage{
+		data:  data,
+		label: strconv.Itoa(elem.Label),
+	}, nil
 }
 
-func (d *Mnist) Close() error {
+func (d *MNIST) Close() error {
 	return nil
 }
 
 func init() {
 	config.AfterInit(func() {
-		mnist = &Mnist{
+		mnist = &MNIST{
 			base: base{
 				ctx: context.Background(),
 			},
---
+++ vision/mnist_test.go
@@ -0,0 +1,38 @@
+package vision
+
+import (
+	"testing"
+
+	context "golang.org/x/net/context"
+
+	"github.com/rai-project/dldataset"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDownloadMNIST(t *testing.T) {
+	ctx := context.Background()
+
+	MNIST, err := dldataset.Get("vision", "mnist")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, MNIST)
+
+	defer MNIST.Close()
+
+	err = MNIST.Download(ctx)
+	assert.NoError(t, err)
+
+	fileList, err := MNIST.List(ctx)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, fileList)
+
+	lbl, err := MNIST.Get(ctx, "train/1")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, lbl)
+
+	lbl, err = MNIST.Get(ctx, "test/1")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, lbl)
+
+	// pp.Println(lbl)
+
+}
