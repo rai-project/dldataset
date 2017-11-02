e1c897b097d16572e298de8f45a04f6c31578cdb
Abdul Dakkak <abduld@wolfram.com>
2017-11-02 13:04:45 -0500

    verks

--- reader/mxnet_recordio.go
+++ reader/mxnet_recordio.go
@@ -1,5 +1,23 @@
 package reader
 
 const (
-	kMagic = uint64(0xced7230a)
+	kMagic = uint32(0xced7230a)
 )
+
+/*!
+ * \brief decode the flag part of lrecord
+ * \param rec the lrecord
+ * \return the flag
+ */
+func decodeFlag(rec uint32) uint32 {
+	return (rec >> uint32(29)) & uint32(7)
+}
+
+/*!
+ * \brief decode the length part of lrecord
+ * \param rec the lrecord
+ * \return the length
+ */
+func decodeLength(rec uint32) uint32 {
+	return rec & ((uint32(1) << uint32(29)) - 1)
+}
