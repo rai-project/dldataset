84af8bcaed84798d5aa920e365611e6d1290f0a3
Abdul Dakkak <abduld@wolfram.com>
2019-03-28 02:25:39 -0500

    add download scripts

---
+++ vision/tools/create_coco_recordio.py
@@ -0,0 +1,4 @@
+
+from gluoncv.data import get_segmentation_dataset
+
+get_segmentation_dataset('coco')
---
+++ vision/tools/download_coco2017.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+dir=$(pwd)
+base_dir=$HOME/data
+mkdir -p ${base_dir}/coco; cd ${base_dir}/coco
+curl -O http://images.cocodataset.org/zips/train2017.zip; unzip train2017.zip
+curl -O http://images.cocodataset.org/zips/val2017.zip; unzip val2017.zip
+curl -O http://images.cocodataset.org/annotations/annotations_trainval2017.zip; unzip annotations_trainval2017.zip
+cd $dir
